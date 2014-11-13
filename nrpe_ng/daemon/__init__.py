34ad774b50d8af6d033030e813d792f30e04b03a
Chris Boot <bootc@bootc.net>
2014-11-13 17:56:29 +0000

    Add nrpe_ng/daemon/daemon_context.py
    This is essentially a copy of daemon/daemon.py from the python-daemon-3K
    package on PyPI.

--- nrpe_ng/daemon/__init__.py
+++ nrpe_ng/daemon/__init__.py
@@ -15,6 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from .daemon_context import DaemonContext
 from .pidfile import PidFile, AlreadyRunningError
 
 __all__ = locals()
